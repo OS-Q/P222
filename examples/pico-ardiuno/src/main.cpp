76b6f6caca5bb5017f70bd925d56c084ff417d29
qitas <qitas@qitas.cn>
2021-04-24 18:01:46 +0800

    fix

---
+++ examples/pico-ardiuno/src/main.cpp
@@ -0,0 +1,20 @@
+
+#include <Arduino.h>
+
+int main(void)
+{
+    Serial.begin(115200, true); // true - retarget stdio printf()
+    pinMode(25, OUTPUT);
+    while (true)
+    {
+        Serial.print("LO");
+        Serial.println("OP");
+        Serial.printf("MILLIS = %u\n", millis());
+        printf("PRINTF %f\n", 22.0 / 7);
+
+        static int led = 0;
+        digitalWrite(25, led);
+        led ^= 1;
+        delay(1000);
+    }
+}
